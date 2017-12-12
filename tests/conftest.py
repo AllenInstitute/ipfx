279e0309b0940c68382f0a46ed3ababf7b4cff73
David Feng <davidf@alleninstitute.org>
2017-12-12 09:26:01 -0800

    ephys tests

---
+++ tests/conftest.py
@@ -0,0 +1,23 @@
+import pytest
+import zipfile
+import numpy as np
+import io
+
+def load_array_from_zip_file(zip_file, file_name):
+    with zipfile.ZipFile(zip_file, 'r') as f:
+        sdata = f.read(file_name)
+        data = np.genfromtxt(io.BytesIO(sdata))
+    return data
+
+
+@pytest.fixture()
+def spike_test_pair():
+    return load_array_from_zip_file("data/spike_test_pair.txt.zip", "spike_test_pair.txt")
+
+@pytest.fixture()
+def spike_test_var_dt():
+    return load_array_from_zip_file("data/spike_test_var_dt.txt.zip", "spike_test_var_dt.txt")
+
+@pytest.fixture()
+def spike_test_high_init_dvdt():
+    return load_array_from_zip_file("data/spike_test_high_init_dvdt.txt.zip", "spike_test_high_init_dvdt.txt")
