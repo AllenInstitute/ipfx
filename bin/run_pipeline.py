ad2198e5a19668301bbe601ec0b2523c10ea4ea3
David Feng <davidf@alleninstitute.org>
2017-12-14 09:40:46 -0800

    pipeline outputs, cell state

--- bin/run_pipeline.py
+++ bin/run_pipeline.py
@@ -5,6 +5,8 @@ from run_sweep_extraction import run_sweep_extraction
 from run_qc import run_qc
 from run_feature_extraction import run_feature_extraction
 
+import allensdk.core.json_utilities as ju
+
 def run_pipeline(input_nwb_file, output_nwb_file, stimulus_ontology_file, qc_fig_dir, qc_criteria):
     se_output = run_sweep_extraction(input_nwb_file,
                                      stimulus_ontology_file)
@@ -19,20 +21,28 @@ def run_pipeline(input_nwb_file, output_nwb_file, stimulus_ontology_file, qc_fig
     for sweep in se_output["sweep_data"]:
         sweep['passed'] = sweep_states[sweep['sweep_number']]['passed']
 
+    se_output['cell_features']['cell_state'] = qc_output['cell_state']
+
     fx_output = run_feature_extraction(input_nwb_file,
                                        output_nwb_file,
                                        qc_fig_dir,
                                        se_output['sweep_data'],
                                        se_output['cell_features'])
 
-    return fx_output
+    return dict( sweep_extraction=se_output, 
+                 qc=qc_output, 
+                 feature_extraction=fx_output )
 
 def main():
     module = ags.ArgSchemaParser(schema_type=PipelineParameters)    
-    run_pipeline(module.args["input_nwb_file"],
-                 module.args["output_nwb_file"],
-                 module.args["stimulus_ontology_file"],
-                 modlue.args["qc_fig_dir"],
-                 module.args["qc_criteria"])
+    output = run_pipeline(module.args["input_nwb_file"],
+                          module.args["output_nwb_file"],
+                          module.args["stimulus_ontology_file"],
+                          module.args["qc_fig_dir"],
+                          module.args["qc_criteria"])
+
+    ju.write(module.args["output_json"], output)
+
+    
 
 if __name__ == "__main__": main()
