a6bb8810711390ad354f3cb76774a02890c7527d
David Feng <davidf@alleninstitute.org>
2018-01-11 12:52:26 -0800

    input resistance test

--- tests/test_ephys_features.py
+++ tests/test_ephys_features.py
@@ -358,3 +358,14 @@ def test_find_stim_interval():
 def test_width_calculation_with_burst():
     # example sp 487663469, sweep 43
     pass
+
+def test_input_resistance():
+    t = np.arange(0, 1.0, 5e-6)
+    v1 = np.ones_like(t) * -5.
+    v2 = np.ones_like(t) * -10.
+    i1 = np.ones_like(t) * -50.
+    i2 = np.ones_like(t) * -100.
+
+    ri = ft.input_resistance([t,t], [i1, i2], [v1,v2], 0, t[-1])
+
+    assert np.allclose(ri, 100.)
