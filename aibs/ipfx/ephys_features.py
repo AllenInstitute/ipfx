refactor fx, runs (possibly wrong values)

aibs/ipfx/ephys_features.py
@@ -1203,13 +1203,14 @@ def find_stim_window(stim, idx0=0):
     return stim_start, stim_end - stim_start
 
 def find_stim_amplitude_and_duration(idx0, stim, hz):
-    vstart = stim[idx0]
+
 
     start, dur = find_stim_window(stim, idx0)
     stim = np.array(stim)[start:start+dur]
 
-    peak_high = max(stim-vstart)
-    peak_low = min(stim-vstart)
+    istart = stim[idx0]
+    peak_high = max(stim-istart)
+    peak_low = min(stim-istart)
 	
     if abs(peak_high) > abs(peak_low):
         amp = float(peak_high)
@@ -1282,6 +1283,148 @@ def _dbl_exp_fit(y0, x, A1, tau1, A2, tau2):
         penalty = 1e6
     return y0 + A1 * np.exp(-x / tau1) + A2 * np.exp(-x / tau2) + penalty
 
+
+def baseline_voltage(t, v, start, baseline_interval=0.1, baseline_detect_thresh=0.3, filter_frequency=1.0):
+    # Look at baseline interval before start if start is defined
+    if start is not None:
+        return average_voltage(v, t, start - baseline_interval, start)
+
+    logging.info("computing baseline voltage interval")
+
+    # Otherwise try to find an interval where things are pretty flat
+    dv = calculate_dvdt(v, t, filter_frequency)
+    non_flat_points = np.flatnonzero(np.abs(dv >= baseline_detect_thresh))
+    flat_intervals = t[non_flat_points[1:]] - t[non_flat_points[:-1]]
+    long_flat_intervals = np.flatnonzero(flat_intervals >= baseline_interval)
+
+    if long_flat_intervals.size > 0:
+        interval_index = long_flat_intervals[0] + 1
+        baseline_end_time = t[non_flat_points[interval_index]]
+        return average_voltage(v, t, baseline_end_time - baseline_interval,
+                               baseline_end_time)
+    else:
+        logging.info("Could not find sufficiently flat interval for automatic baseline voltage", RuntimeWarning)
+        return np.nan
+
+def voltage_deflection(t, v, start, end, deflect_type=None):
+    """Measure deflection (min or max, between start and end if specified).
+
+    Parameters
+    ----------
+    deflect_type : measure minimal ('min') or maximal ('max') voltage deflection
+        If not specified, it will check to see if the current (i) is positive or negative
+        between start and end, then choose 'max' or 'min', respectively
+        If the current is not defined, it will default to 'min'.
+
+    Returns
+    -------
+    deflect_v : peak
+    deflect_index : index of peak deflection
+    """
+
+    deflect_dispatch = {
+        "min": np.argmin,
+        "max": np.argmax,
+    }
+
+    start_index = find_time_index(t, start)
+    end_index = find_time_index(t, end)
+
+    if deflect_type is None:
+        if i is not None:
+            halfway_index = find_time_index(t, (end - start) / 2. + start)
+            if i[halfway_index] >= 0:
+                deflect_type = "max"
+            else:
+                deflect_type = "min"
+        else:
+            deflect_type = "min"
+
+    deflect_func = deflect_dispatch[deflect_type]
+
+    v_window = v[start_index:end_index]
+    deflect_index = deflect_func(v_window) + start_index
+
+    return v[deflect_index], deflect_index,
+
+
+def time_constant(t, v, start, end, frac=0.1, baseline_interval=0.1):
+    """Calculate the membrane time constant by fitting the voltage response with a
+    single exponential.
+
+    Returns
+    -------
+    tau : membrane time constant in seconds
+    """
+
+    # Assumes this is being done on a hyperpolarizing step
+    v_peak, peak_index = voltage_deflection(t, v, start, end, "min")
+    v_baseline = baseline_voltage(t, v, start, baseline_interval=baseline_interval)
+
+    start_index = find_time_index(t, start)
+
+    search_result = np.flatnonzero(v[start_index:] <= frac * (v_peak - v_baseline) + v_baseline)
+    if not search_result.size:
+        raise FeatureError("could not find interval for time constant estimate")
+    fit_start = t[search_result[0] + start_index]
+    fit_end = t[peak_index]
+
+    a, inv_tau, y0 = fit_membrane_time_constant(v, t, fit_start, fit_end)
+
+    return 1. / inv_tau
+
+def sag(t, v, start, end, peak_width=0.005, baseline_interval=0.03):
+    """Calculate the sag in a hyperpolarizing voltage response.
+
+    Parameters
+    ----------
+    peak_width : window width to get more robust peak estimate in sec (default 0.005)
+
+    Returns
+    -------
+    sag : fraction that membrane potential relaxes back to baseline
+    """
+    v_peak, peak_index = voltage_deflection(t, v, start, end, "min")
+    v_peak_avg = average_voltage(v, t, start=t[peak_index] - peak_width / 2.,
+                                 end=t[peak_index] + peak_width / 2.)
+    v_baseline = baseline_voltage(t, v, start, baseline_interval=baseline_interval)
+    v_steady = average_voltage(v, t, start=end - baseline_interval, end=end)
+    sag = (v_peak_avg - v_steady) / (v_peak_avg - v_baseline)
+    return sag
+
+def input_resistance(t_set, i_set, v_set, start, end, baseline_interval=0.1):
+    """Estimate input resistance in MOhms, assuming all sweeps in passed extractor
+    are hyperpolarizing responses."""
+
+    v_vals = []
+    i_vals = []
+    for t, i, v, in zip(t_set, i_set, v_set):
+        v_peak, min_index = voltage_deflection(t, v, start, end, 'min')
+        v_vals.append(v_peak)
+        i_vals.append(i[min_index])
+
+    v = np.array(v_vals)
+    i = np.array(i_vals)
+
+    if len(v) == 1:
+        # If there's just one sweep, we'll have to use its own baseline to estimate
+        # the input resistance
+        v = np.append(v, baseline_voltage(t_set[0], v, start, baseline_interval=baseline_interval))
+        i = np.append(i, 0.)
+
+    A = np.vstack([i, np.ones_like(i)]).T
+    m, c = np.linalg.lstsq(A, v)[0]
+
+    return m * 1e3
+
+def _step_stim_amp(t, i, start):
+    t_index = find_time_index(t, start)
+    return i[t_index + 1]
+
+
+def _short_step_stim_amp(t, i, start):
+    t_index = find_time_index(t, start)
+    return i[t_index + 1:].max()
 
 class FeatureError(Exception):
     """Generic Python-exception-derived object raised by feature detection functions."""

aibs/ipfx/plot_qc_figures.py
@@ -35,14 +35,9 @@ def get_features(sweep_features, sweep_number):
 
 def load_sweep(data_set, sweep_number):
     sweep = data_set.sweep(sweep_number)
-    
-    r = sweep['index_range']
-    v = sweep['response'] * 1e3
-    i = sweep['stimulus'] * 1e12
-    dt = 1.0 / sweep['sampling_rate']
-    t = np.arange(0, len(v)) * dt
-
-    return (v, i, t, r, dt)
+    dt = sweep.t[1] - sweep.t[0]
+    r = sweep.start / dt, sweep.end / dt
+    return (sweep.v, sweep.i, sweep.t, r, dt)
 
 def plot_single_ap_values(data_set, sweep_numbers, lims_features, sweep_features, cell_features, type_name):
     figs = [ plt.figure() for f in range(3+len(sweep_numbers)) ]

aibs/ipfx/stimulus_protocol_analysis.py
@@ -1,40 +1,46 @@
-from . import ephys_extractor as efex
 import numpy as np
+import pandas as pd
 import logging
 from collections import Counter
 
+from . import ephys_features as ft
+from . import ephys_extractor as efex
 
+from .ephys_data_set import SweepSet
+    
 class StimulusProtocolAnalysis(object):
     MEAN_FEATURES = [ "upstroke_downstroke_ratio", "peak_v", "peak_t", "trough_v", "trough_t",
                       "fast_trough_v", "fast_trough_t", "slow_trough_v", "slow_trough_t",
                       "threshold_v", "threshold_i", "threshold_t", "peak_v", "peak_t" ]
 
-    def __init__(self, extractor):
-        self.extractors = { 'all': extractor }
+    def __init__(self, spx, sptx):
+        self.spx = spx
+        self.sptx = sptx
+
         self.features = {}
 
-    @property
-    def subthreshold_extractor(self):
-        if 'subthreshold' not in self.extractors:
-            subthreshold_sweeps = [sweep for sweep in self.extractor.sweeps() if sweep.sweep_feature("avg_rate") == 0]
-            self.add_extractor('subthreshold', subthreshold_sweeps)
+        self._spikes_set = None
+        self._spikes_by_id = {}
+        self._sweep_features = None
 
-        return self.extractors['subthreshold']
+    def _sweep_to_dict(self, sweep):
+        s = sweep.to_dict() 
+        s['spikes'] = self._spikes_by_id[s['id']].to_dict(orient='records')        
+        return s
 
-    @property
-    def suprathreshold_extractor(self):
-        if 'suprathreshold' not in self.extractors:
-            suprathreshold_sweeps = [sweep for sweep in self.extractor.sweeps() if sweep.sweep_feature("avg_rate") > 0]
-            self.add_extractor('suprathreshold', suprathreshold_sweeps)
-            
-        return self.extractors['suprathreshold']
+    def _sweeps_to_dict(self, sweeps):
+        sweeps = sweeps.to_dict(orient="records")
+        for s in sweeps:
+            s['spikes'] = self._spikes_by_id[s['id']].to_dict(orient='records')
+        return sweeps
 
-    def add_extractor(self, name, sweeps):
-        ext = efex.EphysSweepSetFeatureExtractor.from_sweeps(sweeps)
-        self.extractors[name] = ext
-        return ext
+    def subthreshold_sweeps(self, sweep_features):
+        return sweep_features["avg_rate"] == 0
 
-    def mean_features_first_spike(self, sweeps, features_list=None):
+    def suprathreshold_sweeps(self, sweep_features):
+        return sweep_features["avg_rate"] > 0
+
+    def mean_features_first_spike(self, spikes_set, features_list=None):
         """ Compute mean feature values for the first spike in list of extractors """
 
         if features_list is None:
@@ -42,155 +48,166 @@ class StimulusProtocolAnalysis(object):
 
         output = {}
         for mf in features_list:
-            mfd = [ sweep.spikes()[0][mf] for sweep in sweeps if sweep.sweep_feature("avg_rate") > 0 ]
+            mfd = [ spikes[mf].values[0] for spikes in spikes_set if len(spikes) > 0 ]
             output[mf] = np.mean(mfd)
         return output
+    
+    def analyze(self, sweep_set, ids=None, extra_sweep_features=None):
+        if ids is None:
+            ids = range(len(sweep_set))
 
-    @property
-    def extractor(self):
-        return self.extractors['all']
+        self._spikes_set = [ self.spx.process(sweep.t, sweep.v, sweep.i) 
+                             for sweep in sweep_set.sweeps ]
 
-    def analyze(self):
-        pass
+        self._spikes_by_id = { id:self._spikes_set[i] for i,id in enumerate(ids) }
 
+        self._sweep_features = pd.DataFrame([ self.sptx.process(sweep.t, sweep.v, sweep.i, spikes, extra_sweep_features) 
+                                              for sweep, spikes in zip(sweep_set.sweeps, self._spikes_set) ])
+
+            
+        self._sweep_features['id'] = pd.Series(ids)
+        
     def as_dict(self):
         return {}
 
 class RampAnalysis(StimulusProtocolAnalysis):
-    def analyze(self): 
-        self.extractor.process_spikes()
-        spiking_ext = self.suprathreshold_extractor
-        self.features["spiking_sweeps"] = spiking_ext.sweeps()
-        self.features["mean_spike_0"] = self.mean_features_first_spike(spiking_ext.sweeps())
+    def analyze(self, sweep_set, ids=None): 
+        super(RampAnalysis, self).analyze(sweep_set, ids)
+
+        spiking_sweeps = self.suprathreshold_sweeps(self._sweep_features)
+        self.features["spiking_sweeps"] = self._sweep_features[spiking_sweeps]
+        self.features["mean_spike_0"] = self.mean_features_first_spike(self._spikes_set)
 
     def as_dict(self):
         out = self.features.copy()
-        out["spiking_sweeps"] = [ s.as_dict() for s in out["spiking_sweeps"] ]
-        return out
+        for k in [ "spiking_sweeps" ]:
+            out[k] = self._sweeps_to_dict(out[k])
+
+        return self.features
 
 class LongSquareAnalysis(StimulusProtocolAnalysis):
     SUBTHRESH_MAX_AMP = 0
     SAG_TARGET = -100.
     HERO_MIN_AMP_OFFSET = 39.0
-    HERO_MAX_AMP_OFFSET = 61.0
+    HERO_MAX_AMP_OFFSET = 61.0    
 
-    def __init__(self, extractor, subthresh_min_amp):
-        super(LongSquareAnalysis, self).__init__(extractor)
+    def __init__(self, spx, sptx, subthresh_min_amp, baseline_interval=0.3, tau_frac=0.1):
+        super(LongSquareAnalysis, self).__init__(spx, sptx)
         self.subthresh_min_amp = subthresh_min_amp
+        self.sptx.stim_amp_fn = ft._step_stim_amp
+        self.baseline_interval = baseline_interval
+        self.tau_frac = tau_frac
 
-    def analyze(self): 
-        ext = self.extractor
-        ext.process_spikes()
+    def analyze(self, sweep_set, ids=None): 
+        extra_sweep_features = ['peak_deflect','stim_amp','v_baseline','sag','burst','delay']
+        super(LongSquareAnalysis, self).analyze(sweep_set, ids, extra_sweep_features=extra_sweep_features)
 
-        self.features["sweeps"] = ext.sweeps()
-        for s in ext.sweeps():
-            s.set_stimulus_amplitude_calculator(efex._step_stim_amp)
-
-        self.analyze_suprathreshold()
-        self.analyze_subthreshold()
+        self.features["sweeps"] = self._sweep_features
+        self.analyze_suprathreshold(sweep_set)
+        self.analyze_subthreshold(sweep_set)
 
 
-    def analyze_suprathreshold(self):
-        ext = self.extractor
-        spiking_indexes = np.flatnonzero(ext.sweep_features("avg_rate"))
+    def analyze_suprathreshold(self, sweep_set):
+        spiking_sweeps = self.suprathreshold_sweeps(self._sweep_features)
 
-        if len(spiking_indexes) == 0:
+        if len(spiking_sweeps) == 0:
             raise ft.FeatureError("No spiking long square sweeps, cannot compute cell features.")
 
-        amps = ext.sweep_features("stim_amp")#self.long_squares_stim_amps()
-        min_index = np.argmin(amps[spiking_indexes])
-        rheobase_index = spiking_indexes[min_index]
-        rheobase_i = efex._step_stim_amp(ext.sweeps()[rheobase_index])
+        
+        spiking_features = self._sweep_features[spiking_sweeps]
+        min_index = np.argmin(spiking_features["stim_amp"])
+        rheobase_index = spiking_features.loc[min_index].name
+        rheo_sweep = sweep_set.sweeps[rheobase_index]
+        rheobase_i = ft._step_stim_amp(rheo_sweep.t, rheo_sweep.i, self.spx.start)
 
         self.features["rheobase_extractor_index"] = rheobase_index
         self.features["rheobase_i"] = rheobase_i
-        self.features["rheobase_sweep"] = ext.sweeps()[rheobase_index]
+        self.features["rheobase_sweep"] = spiking_features.iloc[min_index]
+        self.features["spiking_sweeps"] = spiking_features
 
-        spiking_ext = self.suprathreshold_extractor
-        self.features["spiking_sweeps"] = spiking_ext.sweeps()
-        self.features["fi_fit_slope"] = efex.fit_fi_slope(spiking_ext)
+        self.features["fi_fit_slope"] = efex.fit_fi_slope(spiking_features["stim_amp"].values,
+                                                          spiking_features["avg_rate"].values)
 
         # find hero sweep
-        hero_sweep = self.find_hero_sweep(rheobase_i, self.suprathreshold_extractor.sweeps())
+        hero_sweep = self.find_hero_sweep(rheobase_i, spiking_features)
         self.features['hero_sweep'] = hero_sweep
 
-    def analyze_subthreshold(self):
-        subthresh_ext = self.subthreshold_extractor
-        subthresh_sweeps = subthresh_ext.sweeps()
+    def analyze_subthreshold(self, sweep_set):
+        subthresh_sweeps = self.subthreshold_sweeps(self._sweep_features)
+        subthresh_features = self._sweep_features[subthresh_sweeps]
 
-        if len(subthresh_sweeps) == 0:
+        if len(subthresh_features) == 0:
             raise ft.FeatureError("No subthreshold long square sweeps, cannot evaluate cell features.")
 
-        peaks = subthresh_ext.sweep_features("peak_deflect")
-        sags = subthresh_ext.sweep_features("sag")
-        sag_eval_levels = np.array([sweep.voltage_deflection()[0] for sweep in subthresh_ext.sweeps()])
-        target_level = self.SAG_TARGET
-        closest_index = np.argmin(np.abs(sag_eval_levels - target_level))
+        logging.debug("subthresh_sweeps: %d", len(subthresh_features))
 
-        self.features["sag"] = sags[closest_index]
+        sags = subthresh_features["sag"]
+        sag_eval_levels = np.array([ v[0] for v in subthresh_features["peak_deflect"] ])
+        closest_index = np.argmin(np.abs(sag_eval_levels - self.SAG_TARGET))
+
+        self.features["sag"] = sags.values[closest_index]
         self.features["vm_for_sag"] = sag_eval_levels[closest_index]
-        self.features["subthreshold_sweeps"] = subthresh_ext.sweeps()
-        for s in self.features["subthreshold_sweeps"]:
-            s.set_stimulus_amplitude_calculator(efex._step_stim_amp)
+        self.features["subthreshold_sweeps"] = subthresh_features
 
-        logging.debug("subthresh_sweeps: %d", len(subthresh_sweeps))
-        calc_subthresh_sweeps = [sweep for sweep in subthresh_sweeps if
-                                 sweep.sweep_feature("stim_amp") < self.SUBTHRESH_MAX_AMP and
-                                 sweep.sweep_feature("stim_amp") > self.subthresh_min_amp]
-        calc_subthresh_ext = self.add_extractor('subthreshold_membrane_property', calc_subthresh_sweeps)
+        calc_subthresh_features = subthresh_features[ (subthresh_features["stim_amp"] < self.SUBTHRESH_MAX_AMP) & \
+                                                      (subthresh_features["stim_amp"] > self.subthresh_min_amp) ].copy()
 
-        logging.debug("calc_subthresh_sweeps: %d", len(calc_subthresh_sweeps))        
+        calc_subthresh_ss = SweepSet([sweep_set.sweeps[i] for i in calc_subthresh_features.index.values])
+        taus = [ ft.time_constant(s.t, s.v, self.spx.start, self.spx.end, self.tau_frac, self.baseline_interval) for s in calc_subthresh_ss.sweeps ]
+        calc_subthresh_features['tau'] = pd.Series(taus)
 
-        self.features["subthreshold_membrane_property_sweeps"] = calc_subthresh_ext.sweeps()
-        self.features["input_resistance"] = efex.input_resistance(calc_subthresh_ext)
-        self.features["tau"] = efex.membrane_time_constant(calc_subthresh_ext)
-        self.features["v_baseline"] = np.nanmean(self.extractor.sweep_features("v_baseline"))
-
+        self.features["subthreshold_membrane_property_sweeps"] = calc_subthresh_features
+        self.features["input_resistance"] = ft.input_resistance(calc_subthresh_ss.t,
+                                                                calc_subthresh_ss.i,
+                                                                calc_subthresh_ss.v,
+                                                                self.spx.start, self.spx.end,
+                                                                self.baseline_interval)
+        
+        self.features["tau"] = np.nanmean(calc_subthresh_features['tau'])
+        self.features["v_baseline"] = np.nanmean(calc_subthresh_features["v_baseline"].values)
 
     def as_dict(self):
         out = self.features.copy()
-        
-        out["sweeps"] = [ s.as_dict() for s in out["sweeps"] ]
-        out["spiking_sweeps"] = [ s.as_dict() for s in out["spiking_sweeps"] ]
-        out["subthreshold_sweeps"] = [ s.as_dict() for s in out["subthreshold_sweeps"] ]
-        out["subthreshold_membrane_property_sweeps"] = [ s.as_dict() for s in out["subthreshold_membrane_property_sweeps"] ]
-        out["rheobase_sweep"] = out["rheobase_sweep"].as_dict()
-        out["hero_sweep"] = out["hero_sweep"].as_dict() if out.get("hero_sweep", None) else None
+
+        for k in [ "sweeps", "subthreshold_membrane_property_sweeps", "subthreshold_sweeps", "spiking_sweeps" ]:
+            out[k] = self._sweeps_to_dict(out[k])
+
+        for k in [ "hero_sweep", "rheobase_sweep" ]:
+            out[k] = self._sweep_to_dict(out[k])
 
         return out
 
-    def find_hero_sweep(self, rheo_amp, long_square_spiking_sweeps, 
+    def find_hero_sweep(self, rheo_amp, spiking_features, 
                         min_offset=HERO_MIN_AMP_OFFSET, 
                         max_offset=HERO_MAX_AMP_OFFSET):
 
         hero_min, hero_max = rheo_amp + min_offset, rheo_amp + max_offset
-        hero_amp = float("inf")
-        hero_sweep = None
+        spiking_features = spiking_features.sort_values("stim_amp")
+        hero_features = spiking_features[(spiking_features["stim_amp"] > hero_min) & (spiking_features["stim_amp"] < hero_max)]
 
-        for sweep in long_square_spiking_sweeps:
-            nspikes = len(sweep.spikes())
-            amp = sweep.sweep_feature("stim_amp")
-
-            if nspikes > 0 and amp > hero_min and amp < hero_max and amp < hero_amp:
-                hero_amp = amp
-                hero_sweep = sweep
-
-        return hero_sweep
-
+        if len(hero_features) == 0:
+            return None
+        else:
+            return hero_features.iloc[0]
 
 
 class ShortSquareAnalysis(StimulusProtocolAnalysis): 
-    def analyze(self):
-        self.extractor.process_spikes()
+    def __init__(self, spx, sptx):
+        super(ShortSquareAnalysis, self).__init__(spx, sptx)
+        self.sptx.stim_amp_fn = ft._short_step_stim_amp
 
-        spiking_ext = self.suprathreshold_extractor
-        spiking_sweeps = spiking_ext.sweeps()
+    def analyze(self, sweep_set, ids=None):
+        extra_sweep_features = ['peak_deflect','stim_amp','v_baseline','sag','burst','delay']
+        super(ShortSquareAnalysis, self).analyze(sweep_set, ids, extra_sweep_features=extra_sweep_features)
+
+        spiking_sweeps = self.suprathreshold_sweeps(self._sweep_features)
+        spiking_features = self._sweep_features[spiking_sweeps]
 
         # Need to count how many had spikes at each amplitude; find most; ties go to lower amplitude
         if len(spiking_sweeps) == 0:
             raise ft.FeatureError("No spiking short square sweeps, cannot compute cell features.")
 
-        most_common = Counter(map(efex._short_step_stim_amp, spiking_sweeps)).most_common()
+        most_common = Counter(spiking_features["stim_amp"].values).most_common()
         common_amp, common_count = most_common[0]
         for c in most_common[1:]:
             if c[1] < common_count:
@@ -198,19 +215,14 @@ class ShortSquareAnalysis(StimulusProtocolAnalysis):
             if c[0] < common_amp:
                 common_amp = c[0]
 
-        ca_sweeps = [sweep for sweep in spiking_sweeps if efex._short_step_stim_amp(sweep) == common_amp]
-        ca_ext = self.add_extractor('common_amp', ca_sweeps)
-
+        ca_features = spiking_features[spiking_features["stim_amp"] == common_amp]
         self.features["stimulus_amplitude"] = common_amp
-        self.features["common_amp_sweeps"] = ca_ext.sweeps()
-
-        for s in ca_ext.sweeps():
-            s.set_stimulus_amplitude_calculator(efex._short_step_stim_amp)
-
-        self.features["mean_spike_0"] = self.mean_features_first_spike(ca_ext.sweeps())
+        self.features["common_amp_sweeps"] = ca_features
+        self.features["mean_spike_0"] = self.mean_features_first_spike(self._spikes_set)
 
             
     def as_dict(self):
         out = self.features.copy()
-        out["common_amp_sweeps"] = [ s.as_dict() for s in out["common_amp_sweeps"] ]
+        for k in [ "common_amp_sweeps" ]:
+            out[k] = self._sweeps_to_dict(out[k])
         return out