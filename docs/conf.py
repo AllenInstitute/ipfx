f947612990299b7340706ce34f4dfd088bde4675
nicholasc <nicain.seattle@gmail.com>
2018-01-25 16:07:16 -0800

    major update to template, WIP until valiated

--- docs/conf.py
+++ docs/conf.py
@@ -40,10 +40,21 @@ import aibs.ipfx
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
-extensions = ['sphinx.ext.autodoc', 'sphinx.ext.viewcode']
+extensions = ['sphinx.ext.autodoc', 
+              'sphinx.ext.viewcode',
+              'sphinx.ext.mathjax',
+              'sphinx_gallery.gen_gallery']
+
+sphinx_gallery_conf = {
+    # path to your examples scripts
+    'examples_dirs' : './gallery',
+    # path where to save gallery generated examples
+    'gallery_dirs'  : 'auto_examples',
+    'filename_pattern': '/',
+    'backreferences_dir': False}
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['aibs_sphinx/templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
@@ -56,7 +67,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'ipfx'
-copyright = u"2017. Allen Institute."
+copyright = u"2018. Allen Institute."
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout
@@ -111,15 +122,17 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'aibs_sphinx'
 
 # Theme options are theme-specific and customize the look and feel of a
 # theme further.  For a list of options available for each theme, see the
 # documentation.
-#html_theme_options = {}
+html_theme_options = {
+    "sidebarwidth": "300"
+}
 
 # Add any paths that contain custom themes here, relative to this directory.
-#html_theme_path = []
+html_theme_path = ['.']
 
 # The name for this set of Sphinx documents.  If None, it defaults to
 # "<project> v<release> documentation".
@@ -142,7 +155,7 @@ html_theme = 'default'
 # here, relative to this directory. They are copied after the builtin
 # static files, so a file named "default.css" will overwrite the builtin
 # "default.css".
-html_static_path = ['_static']
+html_static_path = ['_static', 'aibs_sphinx/static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page
 # bottom, using the given strftime format.
@@ -153,7 +166,7 @@ html_static_path = ['_static']
 #html_use_smartypants = True
 
 # Custom sidebar templates, maps document names to template names.
-#html_sidebars = {}
+html_sidebars = { '**': [ 'globaltoc.html', 'searchbox.html' ]}
 
 # Additional templates that should be rendered to pages, maps page names
 # to template names.
@@ -173,11 +186,11 @@ html_static_path = ['_static']
 
 # If true, "Created using Sphinx" is shown in the HTML footer.
 # Default is True.
-#html_show_sphinx = True
+html_show_sphinx = False
 
 # If true, "(C) Copyright ..." is shown in the HTML footer.
 # Default is True.
-#html_show_copyright = True
+html_show_copyright = False
 
 # If true, an OpenSearch description file will be output, and all pages
 # will contain a <link> tag referring to it.  The value of this option
