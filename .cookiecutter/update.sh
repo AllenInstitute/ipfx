2cb88c25bf3d8681b9e02bd4e4bff46ffd249ef5
nicholasc <nicain.seattle@gmail.com>
2018-01-25 16:07:16 -0800

    major update to template, WIP until valiated

--- .cookiecutter/update.sh
+++ .cookiecutter/update.sh
@@ -1,11 +1,15 @@
+#!/bin/bash
+set -e
+
 PROJECT_DIR=$(pwd)/..
 
-cookiecutter https://github.com/nicain/pyproject_template -o ../.. --config-file .cookiecutter.yaml --no-input --overwrite-if-exists --checkout 25
+python update_from_repo.py
 
 # Add post-cookiecutter commands that you always want run here:
 git checkout -- $PROJECT_DIR/README.md
 git checkout -- $PROJECT_DIR/AUTHORS.rst
-git checkout -- $PROJECT_DIR/requirements.txt
+git checkout -- $PROJECT_DIR/Pipfile
+git checkout -- $PROJECT_DIR/.cookiecutter/update.sh
 
 # Enter patch mode on remaining diffs:
 git add -p
