e33e5a89b7f9741a32a73506149950764a540f26
Thomas Braun <thomas.braun@byte-physics.de>
2018-12-10 16:01:58 +0100

    Tests: Make EphysDataSet and StimulusOntology tests flake8 clean

--- tests/test_ephys_data_set.py
+++ tests/test_ephys_data_set.py
@@ -1,21 +1,20 @@
 import pandas as pd
-import pytest
 from ipfx.stimulus import StimulusOntology
 from ipfx.ephys_data_set import EphysDataSet
 
 
 def test_filtered_sweep_table():
 
-    d = {'bridge_balance_mohm': [15.6288156509,None,None],
-    'clamp_mode': ['CurrentClamp','VoltageClamp','VoltageClamp'],
-    'leak_pa' : [-6.20449542999,None,None],
-    'stimulus_code': ['C1LSFINEST150112', 'EXTPBREAKN141203', 'EXTPBREAKN141203'],
-    'stimulus_code_ext': ['C1LSFINEST150112[0]','EXTPBREAKN141203[0]','EXTPBREAKN141203[0]'],
-    'stimulus_name': ["Long Square","Test","Test"],
-    'stimulus_scale_factor':[10.0,0.5,0.5],
-    'stimulus_units': ['pA','mV','mV'],
-    'sweep_number': [49,5,6],
-    }
+    d = {'bridge_balance_mohm': [15.6288156509, None, None],
+         'clamp_mode': ['CurrentClamp', 'VoltageClamp', 'VoltageClamp'],
+         'leak_pa': [-6.20449542999, None, None],
+         'stimulus_code': ['C1LSFINEST150112', 'EXTPBREAKN141203', 'EXTPBREAKN141203'],
+         'stimulus_code_ext': ['C1LSFINEST150112[0]', 'EXTPBREAKN141203[0]', 'EXTPBREAKN141203[0]'],
+         'stimulus_name': ["Long Square", "Test", "Test"],
+         'stimulus_scale_factor': [10.0, 0.5, 0.5],
+         'stimulus_units': ['pA', 'mV', 'mV'],
+         'sweep_number': [49, 5, 6],
+         }
 
     df = pd.DataFrame(d)
 
