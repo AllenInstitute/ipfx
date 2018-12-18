AllenInstitute/ipfx

commit 2b67c42ac53ef71d3e50fc55e73f92af1a9219e1

EphysDataSet.get_sweep_info_by_sweep_number: Implement with filtered_sweep_table
This makes the code clearer and avoids code duplication.

ipfx/ephys_data_set.py
@@ -28,6 +28,7 @@ class EphysDataSet(object):
                              stimuli=None,
                              exclude_search=False,
                              exclude_test=False,
+                             sweep_number=None,
                              ):
 
         st = self.sweep_table
@@ -50,6 +51,10 @@ class EphysDataSet(object):
 
         if exclude_test:
             mask = ~st[self.STIMULUS_NAME].isin(self.ontology.test_names)
+            st = st[mask]
+
+        if sweep_number:
+            mask = st[self.SWEEP_NUMBER] == sweep_number
             st = st[mask]
 
         return st
@@ -79,11 +84,10 @@ class EphysDataSet(object):
         -------
         sweep_info: dict of sweep properties
         """
-        st = self.sweep_table
-        mask = st[self.SWEEP_NUMBER] == sweep_number
-        st = st[mask]
 
-        return st.to_dict(orient='records')[0]
+        sweeps = self.filtered_sweep_table(sweep_number=sweep_number)
+
+        return sweeps.to_dict(orient='records')[0]
 
     def sweep(self, sweep_number):
         """ returns a dictionary with properties: i (in pA), v (in mV), t (in sec), start, end"""

commit 32dacb113031673fb946b1d399154fab3279f3f1

EphysDataSet: Fix filtered_sweep_table for sweep_number 0
Broken since 2b67c42a (EphysDataSet.get_sweep_info_by_sweep_number:
Implement with filtered_sweep_table, 2018-12-10).

ipfx/ephys_data_set.py
@@ -55,7 +55,7 @@ class EphysDataSet(object):
             mask = ~st[self.STIMULUS_NAME].isin(self.ontology.test_names)
             st = st[mask]
 
-        if sweep_number:
+        if sweep_number is not None:
             mask = st[self.SWEEP_NUMBER] == sweep_number
             st = st[mask]
 

tests/test_ephys_data_set.py
@@ -57,6 +57,13 @@ def test_filtered_sweep_table_works():
 
     assert sweeps["sweep_number"].tolist() == [5, 6]
 
+def test_filtered_sweep_table_works_with_sweep_number():
+
+    ds = get_dataset()
+    sweeps = ds.filtered_sweep_table(sweep_number=0)
+
+    assert sweeps["sweep_number"].tolist() == [0]
+
 
 def test_get_sweep_info():
 