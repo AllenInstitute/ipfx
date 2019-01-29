0b7cb8457bc65bca46ea0b3989c08db05f71e57c
Sergey Gratiy <sergeyg@alleninstitute.org>
2019-01-29 15:24:13 -0800

    Fix bug with incorrect extractor import

--- tests/test_stimulus_protocol_analysis.py
+++ tests/test_stimulus_protocol_analysis.py
@@ -1,8 +1,8 @@
 import pytest
 import pandas as pd
 import numpy as np
-import ipfx.stimulus_protocol_analysis as spa
-import ipfx.ephys_extractor as efex
+from ipfx.stimulus_protocol_analysis import LongSquareAnalysis
+from ipfx.feature_extractor import SpikeTrainFeatureExtractor, SpikeFeatureExtractor
 
 
 @pytest.fixture()
@@ -12,13 +12,13 @@ def spiking_sweep_features():
 
     return df
 
+
 @pytest.fixture()
 def long_square_analysis():
     # build the extractors
-    spx = efex.SpikeExtractor(start=0.27, end=1.27)
-    spfx = efex.SpikeTrainFeatureExtractor(start=0.27, end=1.27)
-
-    return spa.LongSquareAnalysis(spx, spfx, subthresh_min_amp=-100.0)
+    spx = SpikeFeatureExtractor(start=0.27, end=1.27)
+    spfx = SpikeTrainFeatureExtractor(start=0.27, end=1.27)
+    return LongSquareAnalysis(spx, spfx, subthresh_min_amp=-100.0)
 
 
 def test_find_rheobase_sweep(long_square_analysis, spiking_sweep_features):
@@ -26,7 +26,6 @@ def test_find_rheobase_sweep(long_square_analysis, spiking_sweep_features):
     spiking_features_with_negative_amplitude = spiking_sweep_features
     rheobase_sweep = long_square_analysis.find_rheobase_sweep(spiking_features_with_negative_amplitude)
     assert rheobase_sweep["stim_amp"] == 20
-
 
 def test_find_hero_sweep(long_square_analysis, spiking_sweep_features):
 
