c5a60b9b2040f0738f86ff057b5efd32e245ce20
Sergey Gratiy <sergeyg@alleninstitute.org>
2019-03-05 10:59:07 -0800

    Add test for counting sweep states
    Moved sweep state counter into sweep_props module
    Add a test for counting sweep states

--- ipfx/qc_feature_evaluator.py
+++ ipfx/qc_feature_evaluator.py
@@ -2,6 +2,7 @@ import os, json
 import logging
 import numpy as np
 import ipfx.qc_features as qcf
+import ipfx.sweep_props as sp
 
 DEFAULT_QC_CRITERIA_FILE = os.path.join(os.path.dirname(__file__), 'defaults/qc_criteria.json')
 
@@ -39,7 +40,7 @@ def qc_experiment(ontology, cell_features, sweep_features, qc_criteria=None):
 
     sweep_states = qc_sweeps(ontology,sweep_features,qc_criteria)
 
-    num_passed_sweeps, num_sweeps = count_sweeps(sweep_states)
+    num_passed_sweeps, num_sweeps = sp.count_sweep_states(sweep_states)
 
     if num_sweeps == 0:
         cell_state["fail_tags"].append("No sweep states available")
@@ -51,29 +52,6 @@ def qc_experiment(ontology, cell_features, sweep_features, qc_criteria=None):
 
     return cell_state, sweep_states
 
-
-def count_sweeps(sweep_states):
-    """
-    Count passed and total sweeps
-
-    Parameters
-    ----------
-    sweep_states: list of dicts
-
-    Returns
-    -------
-    num_passed_sweeps: int
-    num_sweeps: int
-
-    """
-    num_passed_sweeps = 0
-    for ss in sweep_states:
-        if ss["passed"] is True:
-            num_passed_sweeps += 1
-
-    num_sweeps = len(sweep_states)
-
-    return num_passed_sweeps, num_sweeps
 
 
 def qc_sweeps(ontology,sweep_features,qc_criteria):
