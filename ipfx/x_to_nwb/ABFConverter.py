AllenInstitute/ipfx

commit ebcfc2326ca5d92a4844176f882100784fd3abb9

ABFConverter.check(): Enhance exception message

ipfx/x_to_nwb/ABFConverter.py
@@ -197,7 +197,7 @@ class ABFConverter:
             elif self.refabf.abfVersion != abf.abfVersion:
                 raise ValueError("abfVersion does not match.")
             elif self.refabf.channelList != abf.channelList:
-                raise ValueError("channelList does not match.")
+                raise ValueError(f"channelList does not match ({self.refabf.channelList} vs {abf.channelList}.")
 
     def _getOldestABF(self):
         """

commit afd2d492e05c585d2c5ececce10f75eb5a509097

ABFConverter._checkAll(): Fix typo in exception message
Broken since ebcfc232 (ABFConverter.check(): Enhance exception message,
2019-02-27).

ipfx/x_to_nwb/ABFConverter.py
@@ -199,7 +199,7 @@ class ABFConverter:
             elif self.refabf.abfVersion != abf.abfVersion:
                 raise ValueError("abfVersion does not match.")
             elif self.refabf.channelList != abf.channelList:
-                raise ValueError(f"channelList does not match ({self.refabf.channelList} vs {abf.channelList}.")
+                raise ValueError(f"channelList does not match ({self.refabf.channelList} vs {abf.channelList}).")
 
     def _getOldestABF(self):
         """