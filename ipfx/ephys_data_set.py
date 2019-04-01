AllenInstitute/ipfx

commit e4575c10e2f0c1a7dde882c6a70ed0ea18926469

Determine clamp_mode from stimulus_units on the fly

ipfx/ephys_data_set.py
@@ -15,6 +15,7 @@ class EphysDataSet(object):
     STIMULUS_NAME = 'stimulus_name'
     SWEEP_NUMBER = 'sweep_number'
     PASSED = 'passed'
+
     CLAMP_MODE = 'clamp_mode'
 
     LONG_SQUARE = 'long_square'
@@ -119,10 +120,12 @@ class EphysDataSet(object):
         stimulus = sweep_data['stimulus'][sweep_start_ix:sweep_end_ix+1]
         t = np.arange(sweep_start_ix, sweep_end_ix + 1) * dt
 
-        if sweep_meta_data[self.CLAMP_MODE] == "VoltageClamp":
+        clamp_mode = self.get_clamp_mode(sweep_meta_data['stimulus_units'])
+
+        if clamp_mode == "VoltageClamp":
             v = stimulus
             i = response
-        elif sweep_meta_data[self.CLAMP_MODE] == "CurrentClamp":
+        elif clamp_mode == "CurrentClamp":
             v = response
             i = stimulus
         else:
@@ -141,6 +144,12 @@ class EphysDataSet(object):
             raise
 
         return sweep
+
+    def get_clamp_mode(self, stimulus_unit):
+
+        clamp_mode = "CurrentClamp" if stimulus_unit in self.ontology.current_clamp_units else "VoltageClamp"
+
+        return clamp_mode
 
     def sweep_set(self, sweep_numbers):
         try:

commit cd7b5a1ff336ed84fc11be27150664ce21da9ac2

EphysDataSet: Fix list of valid current clamp stimulus units
The HBGDataSet uses A as stimulus units and due to the way
EphysDataSet.get_clamp_mode works this resulted in the wrong clamp mode
being determined for the sweeps.

Broken since e4575c10 (Determine clamp_mode from stimulus_units on
the fly, 2019-01-31).

ipfx/ephys_data_set.py
@@ -35,7 +35,7 @@ class EphysDataSet(object):
     VOLTAGE_CLAMP = "VoltageClamp"
     CURRENT_CLAMP = "CurrentClamp"
 
-    current_clamp_units = ('Amps', 'pA')
+    current_clamp_units = ('Amps', 'pA', 'A')
 
     def __init__(self, ontology=None, validate_stim=True):
         self.sweep_table = None