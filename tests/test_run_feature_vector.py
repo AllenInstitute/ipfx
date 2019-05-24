9e7dafdc0cc75732028fdd753b88a5c0a37096f5
Sergey Gratiy <sergeyg@alleninstitute.org>
2019-05-24 16:45:49 -0700

    Add tests for run_feature_vector_extraction and run_feature_collection

---
+++ tests/test_run_feature_vector.py
@@ -0,0 +1,57 @@
+import os
+import numpy as np
+from ipfx.bin.run_feature_vector_extraction import run_feature_vector_extraction
+from ipfx.bin.run_feature_collection import run_feature_collection
+import pandas as pd
+import pytest
+import ipfx.bin.lims_queries as lq
+
+
+TEST_OUTPUT_DIR = "/allen/aibs/informatics/module_test_data/ipfx/test_feature_vector"
+
+if not lq.able_to_connect_to_lims():
+    pytest.skip("cannot connect to LIMS", allow_module_level=True)
+
+
+@pytest.mark.regression
+def test_feature_vector_extraction(tmpdir_factory):
+
+    temp_output_dir = str(tmpdir_factory.mktemp("feature_vector"))
+    test_output_dir = TEST_OUTPUT_DIR
+
+    features = [
+        "first_ap",
+        "isi_shape",
+        "spiking",
+        "step_subthresh",
+        "subthresh_norm",
+        ]
+
+    run_feature_vector_extraction(ids=[500844783, 509604672],
+                                  output_dir=temp_output_dir)
+
+    for feature in features:
+        test_data = np.load(os.path.join(test_output_dir, "fv_{:s}_T301.npy".format(feature)))
+        temp_data = np.load(os.path.join(temp_output_dir, "fv_{:s}_T301.npy".format(feature)))
+
+        assert np.array_equal(test_data, temp_data)
+
+
+@pytest.mark.regression
+def test_feature_collection(tmpdir_factory):
+
+    temp_output_dir = str(tmpdir_factory.mktemp("feature_vector"))
+    test_output_dir = TEST_OUTPUT_DIR
+
+    temp_output_file = os.path.join(temp_output_dir, "features_T301.csv")
+    test_output_file = os.path.join(test_output_dir, "features_T301.csv")
+
+    run_feature_collection(ids=[500844783, 509604672],
+                           output_file=temp_output_file)
+
+    test_table = pd.read_csv(test_output_file, sep=",")
+    temp_table = pd.read_csv(temp_output_file, sep=",")
+
+    assert test_table.equals(temp_table)
+
+
