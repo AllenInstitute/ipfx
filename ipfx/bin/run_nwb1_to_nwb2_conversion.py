7a384a6307861eb0145f7e0c7d1331233201b035
Sergey Gratiy <sergeyg@alleninstitute.org>
2019-08-09 16:18:10 -0700

    Change make_nwb2_file_name to  accept dir_name and base_name
    Allows to create the full name with a different dirname
    Needed for test saved in the temp_dir

--- ipfx/bin/run_nwb1_to_nwb2_conversion.py
+++ ipfx/bin/run_nwb1_to_nwb2_conversion.py
@@ -8,20 +8,8 @@ class ConvertNWBParameters(args.ArgSchema):
     input_nwb_file = args.fields.InputFile(description="input nwb1 file", required=True)
 
 
+def make_nwb2_file_name(dir_name,base_name):
 
-
-
-def convert(nwb1_file,nwb2_file):
-    """
-    """
-
-    NWBConverter(nwb1_file,nwb2_file)
-
-
-def make_nwb2_file_name(nwb1_file_name):
-
-    dir_name = os.path.dirname(nwb1_file_name)
-    base_name = os.path.basename(nwb1_file_name)
     file_name, file_extension = os.path.splitext(base_name)
     nwb2_file_name = os.path.join(dir_name, file_name+"_ver2" + file_extension)
 
@@ -33,15 +21,16 @@ def main():
     module = args.ArgSchemaParser(schema_type=ConvertNWBParameters)
 
     nwb1_file_name = module.args["input_nwb_file"]
-    nwb2_file_name = make_nwb2_file_name(nwb1_file_name)
+    dir_name = os.path.dirname(nwb1_file_name)
+    base_name = os.path.basename(nwb1_file_name)
 
+    nwb2_file_name = make_nwb2_file_name(dir_name,base_name)
 
     if not os.path.exists(nwb1_file_name):
         raise ValueError(f"The file {nwb1_file_name} does not exist.")
 
-
-    convert(nwb1_file = nwb1_file_name,
-            nwb2_file = nwb2_file_name)
+    NWBConverter(input_file = nwb1_file_name,
+                 output_file= nwb2_file_name)
 
 
 if __name__ == "__main__":
