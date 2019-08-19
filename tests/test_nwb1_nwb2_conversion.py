b021ec9f0b1dd3e278d224c377e76390bedf0395
Sergey Gratiy <sergeyg@alleninstitute.org>
2019-08-19 15:36:46 -0700

    Add regression test with patched object_id
    Object_id is unique each time object is created.
    Thus new nwb file with the same data will have different object_id.
    Replace object id with fixed string for regression testing.

--- tests/test_nwb1_nwb2_conversion.py
+++ tests/test_nwb1_nwb2_conversion.py
@@ -4,6 +4,35 @@ import pytest
 from ipfx.x_to_nwb.NWBConverter import NWBConverter
 from .helpers_for_tests import diff_h5, validate_nwb
 from ipfx.bin.run_nwb1_to_nwb2_conversion import make_nwb2_file_name
+from hdmf import Container
+
+
+class TestNWBConverter(NWBConverter):
+
+    @staticmethod
+    def undefined_object_id(self):
+        """
+        Monkey patching object_id property to set to a fixed value. overriding
+        Reassigning Container.object_id is needed for regression testing because
+        object_id is unique to created object rather than to data.
+
+        Parameters
+        ----------
+        self
+
+        Returns
+        -------
+
+        """
+        return "Undefined"
+
+    def __init__(self, input_file, output_file):
+
+        Container.object_id = property(self.undefined_object_id)
+
+        NWBConverter.__init__(self, input_file, output_file)
+
+
 
 
 @pytest.mark.parametrize('NWB_file_inhouse', ['Pvalb-IRES-Cre;Ai14-406663.04.01.01.nwb',
@@ -22,12 +51,12 @@ def test_file_level_regressions(NWB_file_inhouse,tmpdir_factory):
     assert os.path.isfile(nwb1_file_name)
     assert os.path.isfile(test_nwb2_file_name)
 
-    NWBConverter(input_file = nwb1_file_name,
-                 output_file = temp_nwb2_file_name)
+    TestNWBConverter(input_file=nwb1_file_name,
+                     output_file=temp_nwb2_file_name,
+                     )
 
     assert validate_nwb(temp_nwb2_file_name) == []
-
-
+    assert diff_h5(temp_nwb2_file_name,test_nwb2_file_name) == 0
 
 
 
