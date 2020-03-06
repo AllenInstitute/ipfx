30a300ad4220851c33f7cdcdaae93a495e90f265
gnayuy <gnayuy@gmail.com>
2020-03-06 10:22:39 -0800

    add test_labnotebook

---
+++ tests/test_labnotebook.py
@@ -0,0 +1,24 @@
+from __future__ import print_function
+from __future__ import absolute_import
+from builtins import range
+import os
+import pytest
+import h5py
+import numpy as np
+from ipfx.dataset.labnotebook import LabNotebookReaderIgorNwb
+from tests.helpers_for_tests import compare_dicts
+from allensdk.api.queries.cell_types_api import CellTypesApi
+
+TEST_DATA_PATH = os.path.join(os.path.dirname(__file__), 'data')
+
+@pytest.mark.parametrize('NWB_file', ['H18.03.315.11.11.01.05.nwb'], indirect=True)
+def test_labnotebook_reader(NWB_file):
+
+    reader = LabNotebookReaderIgorNwb(NWB_file)
+    assert isinstance(reader, LabNotebookReaderIgorNwb)
+
+    assert reader.get_numerical_keys()[0][1] == 'TimeStamp'
+    np.testing.assert_almost_equal(reader.get_numerical_values()[0][1][0], 3617183992.147)
+
+    assert reader.get_textual_keys()[0][1] == 'TimeStamp'
+    np.testing.assert_almost_equal(float(reader.get_textual_values()[0][1][0]), 3617200000.0)
