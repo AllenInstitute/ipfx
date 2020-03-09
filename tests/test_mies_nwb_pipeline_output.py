57077a813292df13f357567863baf5e548b4d8ee
nilegraddis <nilegraddis@gmail.com>
2020-03-09 14:21:30 -0700

    #351 rebase stimulus ontology path in mies pipeline integration test

--- tests/test_mies_nwb_pipeline_output.py
+++ tests/test_mies_nwb_pipeline_output.py
@@ -1,3 +1,4 @@
+from pathlib import Path
 from builtins import str
 import pytest
 import pandas as pd
@@ -12,6 +13,32 @@ TEST_SPECIMENS_FILE = resource_filename(__name__, 'test_mies_nwb2_specimens.csv'
 
 test_specimens = pd.read_csv(TEST_SPECIMENS_FILE, sep=" ")
 test_specimens_params = [tuple(sp) for sp in test_specimens.values]
+
+
+def rebase(new_base: str, path: str, mkdir: bool = False) ->  str:
+    """Utility for joining a path prefix onto a (potentially absolute) path
+
+    Parameters
+    ----------
+    new_base : Will prefix the output
+    path : to be prefixed
+    mkdir : if True, make all ancestors of the resulting path
+
+    Returns
+    -------
+    The joined path, as a string.
+
+    """
+
+    path = Path(path)
+    if path.is_absolute:
+        path = Path(*path.parts[1:])
+
+    new_path = Path(new_base) / path
+    if mkdir:
+        new_path.parent.mkdir(parents=True, exist_ok=True)
+
+    return str(new_path)
 
 
 @pytest.mark.requires_inhouse_data
@@ -40,10 +67,14 @@ def test_mies_nwb_pipeline_output(input_json, output_json, tmpdir_factory):
     pipeline_input["output_nwb_file"] = os.path.join(test_dir, "output.nwb")  # Modify path for the test output
     pipeline_input["qc_figs_dir"] = None
 
+    stimulus_ontology_file = pipeline_input.get("stimulus_ontology_file", None)
+    if stimulus_ontology_file is not None:
+        stimulus_ontology_file = rebase(test_dir, stimulus_ontology_file, True)
+
     obtained = run_pipeline(pipeline_input["input_nwb_file"],
                                         pipeline_input.get("input_h5_file", None),
                                         pipeline_input["output_nwb_file"],
-                                        pipeline_input.get("stimulus_ontology_file", None),
+                                        stimulus_ontology_file,
                                         pipeline_input["qc_figs_dir"],
                                         pipeline_input["qc_criteria"],
                                         pipeline_input["manual_sweep_states"])
