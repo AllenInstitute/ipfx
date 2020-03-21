1d32d8679ff208a8cb904afac860f254a4857155
Sergey Gratiy <sergeyg@alleninstitute.org>
2020-03-20 23:09:18 -0700

    Add tests for EphysNWBData

---
+++ tests/dataset/test_ephys_nwb_data.py
@@ -0,0 +1,149 @@
+import pytest
+from ipfx.stimulus import StimulusOntology
+from ipfx.dataset.ephys_nwb_data import EphysNWBData
+import allensdk.core.json_utilities as ju
+import datetime
+import pynwb
+from pynwb.icephys import CurrentClampStimulusSeries, CurrentClampSeries
+import numpy as np
+
+from dictdiffer import diff
+
+@pytest.fixture
+def tmp_nwb_path(tmpdir_factory):
+    nwb = tmpdir_factory.mktemp("test_nwb_data").join("test_ephys_data.nwb")
+    return str(nwb)
+
+
+def nwbfile_to_test():
+    """
+    Create a simple nwbfile for testing
+
+    Returns
+    -------
+    nwbfile
+    """
+
+    nwbfile = pynwb.NWBFile(
+        session_description="test nwb data",
+        identifier='test session',
+        session_start_time=datetime.datetime.now(),
+        file_create_date=datetime.datetime.now()
+    )
+
+    device = nwbfile.create_device(name='electrode_0')
+
+    electrode = nwbfile.create_icephys_electrode(
+        name="electrode 0",
+        description='fancy electrode',
+        device=device)
+
+    stimulus_data = [0, 3, 3, 3, 0]
+
+    stimulus_meta_data = {
+        "name": "stimulus",
+        "sweep_number": 4,
+        "unit": "A",
+        "gain": 32.0,
+         "resolution": 1.0,
+         "conversion": 1.0E-3,
+         "starting_time": 1.5,
+         "rate": 7000.0,
+         "stimulus_description": "STIMULUS_CODE"
+    }
+
+    stimulus_series = CurrentClampStimulusSeries(
+        data=stimulus_data,
+        electrode=electrode,
+        **stimulus_meta_data
+    )
+
+    nwbfile.add_stimulus(stimulus_series)
+
+    response_data = [1, 2, 3, 4, 5]
+    response_meta_data = {
+        "name":"acquisition",
+         "sweep_number": 4,
+         "unit": "V",
+         "gain": 32.0,
+         "resolution": 1.0,
+         "conversion": 1.0E-3,
+         "bridge_balance": 500.0,
+         "bias_current":100.0,
+         "starting_time": 1.5,
+         "rate": 7000.0,
+         "stimulus_description": "STIMULUS_CODE"
+    }
+
+    acquisition_series = CurrentClampSeries(data=response_data,
+                                             electrode=electrode,
+                                             **response_meta_data
+                                             )
+
+    nwbfile.add_acquisition(acquisition_series)
+
+    return nwbfile
+
+
+@pytest.fixture
+def nwb_data(tmp_nwb_path):
+
+    nwbfile = nwbfile_to_test()
+    with pynwb.NWBHDF5IO(path=tmp_nwb_path, mode="w") as writer:
+        writer.write(nwbfile)
+
+    ontology =  StimulusOntology(
+        [[('name', 'expected name'), ('code', 'STIMULUS_CODE')],
+         [('name', 'test name'), ('code', 'extpexpend')]
+         ])
+
+    return EphysNWBData(nwb_file=tmp_nwb_path, ontology=ontology)
+
+
+def test_get_stimulus_unit(nwb_data):
+    assert nwb_data.get_stimulus_unit(sweep_number=4) == "amperes"
+
+
+def test_get_stimulus_code(nwb_data):
+    assert nwb_data.get_stimulus_code(sweep_number=4) == "STIMULUS_CODE"
+
+
+def test_get_sweep_data(nwb_data):
+
+    expected = {
+        'stimulus': np.array([0, 3, 3, 3, 0])*1E-3,
+        'response': np.array([1, 2, 3, 4, 5])*1E-3,
+        'sampling_rate': 7000.0,
+        'stimulus_unit': 'amperes'
+    }
+
+    obtained = nwb_data.get_sweep_data(sweep_number=4)
+
+    assert list(diff(expected, obtained, tolerance=0.001)) == []
+
+
+def test_get_sweep_attrs(nwb_data):
+
+    expected = {
+        'stimulus_description': 'STIMULUS_CODE',
+        'sweep_number': 4,
+        'bias_current': 100.0,
+        'bridge_balance': 500.0,
+        'gain': 32.0,
+        'clamp_mode': 'CurrentClamp',
+    }
+
+    obtained = nwb_data.get_sweep_attrs(sweep_number=4)
+    print(obtained)
+    print(expected)
+    assert expected == obtained
+
+def test_get_clamp_mode(nwb_data):
+
+    attrs = nwb_data.get_sweep_attrs(4);
+
+    assert attrs['clamp_mode'] == "CurrentClamp"
+
+def test_get_full_recording_date(nwb_data):
+    assert nwb_data.get_full_recording_date() == nwb_data.nwb.session_start_time
+
