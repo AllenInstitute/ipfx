fa4ada889eec9b431369d1ddd55321bf057f1e46
nile graddis <NileGraddis@gmail.com>
2020-03-27 13:46:38 -0700

    #384 get lsq_analysis running

--- docs/gallery/analysis_examples/lsq_analysis.py
+++ docs/gallery/analysis_examples/lsq_analysis.py
@@ -4,14 +4,12 @@ Long Square Analysis
 
 Detect Long Square Features
 """
-from __future__ import print_function
-
-from builtins import str
 from ipfx.aibs_data_set import AibsDataSet
 from ipfx.feature_extractor import (
     SpikeFeatureExtractor, SpikeTrainFeatureExtractor
 )
 import ipfx.stimulus_protocol_analysis as spa
+from ipfx.epochs import get_stim_epoch
 
 from allensdk.api.queries.cell_types_api import CellTypesApi
 
@@ -29,18 +27,37 @@ sweep_info = ct.get_ephys_sweeps(specimen_id)
 
 # build a data set and find the long squares
 data_set = AibsDataSet(sweep_info=sweep_info, nwb_file=nwb_file)
-lsq_table = data_set.filtered_sweep_table(stimuli=data_set.ontology.long_square_names)
+lsq_table = data_set.filtered_sweep_table(
+    stimuli=data_set.ontology.long_square_names
+)
 lsq_set = data_set.sweep_set(lsq_table.sweep_number)
 
+# find the start and end time of the stimulus 
+# (in this case, this is the same across long-square sweeps, 
+# so we'll just use the first)
+stim_start_index, stim_end_index = get_stim_epoch(lsq_set.i[0])
+stim_start_time = lsq_set.t[0][stim_start_index]
+stim_end_time = lsq_set.t[0][stim_end_index]
+
 # build the extractors
-spx = SpikeFeatureExtractor(start=0.27, end=1.27)
-spfx = SpikeTrainFeatureExtractor(start=0.27, end=1.27)
+spx = SpikeFeatureExtractor(start=stim_start_time, end=stim_end_time)
+spfx = SpikeTrainFeatureExtractor(start=stim_start_time, end=stim_end_time)
 
 # run the analysis and print out a few of the features
 lsqa = spa.LongSquareAnalysis(spx, spfx, subthresh_min_amp=-100.0)
 data = lsqa.analyze(lsq_set)
 
-for field in [ 'tau', 'v_baseline', 'input_resistance', 'vm_for_sag', 'fi_fit_slope', 'sag', 'rheobase_i' ]:
+fields_to_print = [
+    'tau', 
+    'v_baseline', 
+    'input_resistance', 
+    'vm_for_sag', 
+    'fi_fit_slope', 
+    'sag', 
+    'rheobase_i'
+]
+
+for field in fields_to_print:
     print("%s: %s" % (field, str(data[field])))
 
 # plot stim amp vs. firing rate
@@ -50,7 +67,5 @@ plt.plot(spiking_sweeps.stim_amp,
 plt.xlabel('Stimulus amplitude (pA)')
 plt.ylabel('Average firing rate (Hz)')
 
-
-
-
+plt.show()
 
