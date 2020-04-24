a68f354a5b8e04b966840460223e82c5add61eb1
Matt Aitken <matthewaitken2@gmail.com>
2020-04-24 12:41:42 -0700

    adds data and unit arguments to spiketimes TimeSeries object (#405)
    * adds data and unit arguments to spiketimes TimeSeries object
    
    * stores data as np.array to avoid errors when spike times is []

--- ipfx/nwb_append.py
+++ ipfx/nwb_append.py
@@ -9,6 +9,7 @@ import pynwb
 from hdmf.backends.hdf5.h5_utils import H5DataIO
 from pynwb import TimeSeries
 from pynwb import ProcessingModule
+import numpy as np
 
 
 PathLike = Union[
@@ -50,9 +51,11 @@ def append_spike_times(input_nwb_path: PathLike,
     spike_module = ProcessingModule(name='spikes',
                                     description='detected spikes')
     for sweep_num, spike_times in sweep_spike_times.items():
-        wrapped_spike_times = H5DataIO(data=spike_times,
+        wrapped_spike_times = H5DataIO(data=np.asarray(spike_times),
                                        compression=True)
         ts = TimeSeries(timestamps=wrapped_spike_times,
+                        unit='seconds',
+                        data=wrapped_spike_times,
                         name=f"Sweep_{sweep_num}")
         spike_module.add_data_interface(ts)
 
