5fb91d546f04c6f1426d2bd7ff700a87ab35c427
ru57y34nn <mannrusty@gmail.com>
2021-04-14 08:46:57 -0700

    Check if stimulus end index is less than stimulus start index
    If the recording is stopped before completing the stimulus epoch then
    the ending index of the stimulus epoch will be less than the starting
    index of the stimulus epoch and with the changes from my last commit,
    this would prevent the sweep from being tagged with 'Stimulus contains
    NaN values' because there would be no stimulus epoch to check for NaN's.
    This commit checks the indices of the start and end of the stimulus epoch
    and if the end index is less than the start index then the stimulus epoch
    was not completed and the sweep is tagged.

--- ipfx/qc_feature_extractor.py
+++ ipfx/qc_feature_extractor.py
@@ -307,8 +307,12 @@ def check_sweep_integrity(sweep, is_ramp):
             if sweep.epochs["recording"][1] < sweep.epochs["experiment"][1]:
                 tags.append("Recording stopped before completing the experiment epoch")
 
-    sweep_stim = sweep.i[sweep.epochs["stim"][0] : sweep.epochs["stim"][1]]
-    if np.isnan(sweep_stim).any():
+    stim_start_idx = sweep.epochs['stim'][0]
+    stim_end_idx = sweep.epochs['stim'][1]
+    sweep_stim = sweep.i[stim_start_idx : stim_end_idx]
+    if stim_start_idx > stim_end_idx:
+        tags.append("Stimulus contains NaN values")
+    elif np.isnan(sweep_stim).any():
         tags.append("Stimulus contains NaN values")
 
     return tags
