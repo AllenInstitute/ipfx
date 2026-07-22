f48833c33b41ee4ee8637dbba24b8ab8dfbf5ef4
Nathan Gouwens <nathang@alleninstitute.org>
2026-07-22 10:43:08 -0700

    Add placeholder for feature vector extraction runner script

---
+++ ipfx/bin/run_feature_vector_extraction_flex.py
@@ -0,0 +1,54 @@
+import logging
+import os
+import json
+
+import argschema as ags
+import numpy as np
+
+
+class CollectFeatureVectorParameters(ags.ArgSchema):
+    output_dir = ags.fields.OutputDir(
+        description="Destination directory for output files",
+        default="."
+    )
+    specimen_id_file = ags.fields.InputFile(
+        description=("Input file of specimen IDs (one per line)"),
+    )
+    nwb_path_file = ags.fields.InputFile(
+        description=("File with paths to each specimen's NWB file - "
+            "if not supplied, LIMS will be queried for them"),
+        default=None,
+        allow_none=True,
+    )
+    sweep_qc_record_file = ags.fields.InputFile(
+        description=("File with sweep QC status and tags - "
+            "if not supplied, LIMS will be queried for them"),
+        default=None,
+        allow_none=True,
+    )
+    manual_fail_sweep_file = ags.fields.InputFile(
+        description=("File with manual sweep failure information"),
+    )
+    sweep_qc_option = ags.fields.String(
+        description=("Sweep-level QC option - "
+            "'none': use all sweeps; "
+            "'lims-passed-only': check passed status with LIMS and "
+            "only used passed sweeps "
+            "'lims-passed-except-delta-vm': check status with LIMS and "
+            "use passed sweeps and sweeps where only failure criterion is delta_vm"
+            "'lims-passed-except-delta-vm-and-rms': check status with LIMS and "
+            "use passed sweeps and sweeps where only failure criterion is delta_vm,"
+            "but also re-calculate RMS values with current code"
+            ),
+        default='none'
+    )
+
+
+def main(args):
+    with open(module.args["specimen_id_file"], "r") as f:
+        ids = [int(line.strip("\n")) for line in f]
+
+
+if __name__ == "__main__":
+    module = ags.ArgSchemaParser(schema_type=CollectFeatureVectorParameters)
+    main(module.args)
